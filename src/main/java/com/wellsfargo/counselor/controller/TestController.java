4a14e77e36e2994f4cad2cac56cafc8c8fecdbf0
Aditya Byereddy <abyeredd@asu.edu>
2025-12-15 05:56:44 +0000

    2nd commit: Update Java version and add TestController

---
+++ src/main/java/com/wellsfargo/counselor/controller/TestController.java
@@ -0,0 +1,18 @@
+package com.wellsfargo.counselor.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Wells Fargo Financial Advisor System is running!";
+    }
+
+    @GetMapping("/api/test")
+    public String test() {
+        return "API is working!";
+    }
+}
